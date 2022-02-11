d5eb71ef29fabacdb3834d2d1e6d6798ba38b58e
Manager <timokhina.work@gmail.com>
2022-02-11 12:55:20 +0300

    ver 1.01

--- src/main/java/pw/affstaging/admin/HelloWorld.java
+++ src/main/java/pw/affstaging/admin/HelloWorld.java
@@ -4,4 +4,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+
+    public void test(){
+        String test = "внесение правок в мастер ветке";
+                System.out.printf(test);
+
+
+    }
+
 }
